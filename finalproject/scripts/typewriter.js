7d9ce3f708e9672a3b04bb2bd41a104288a6e575
Carlosjunc0 <cjunco4u@gmail.com>
2025-12-03 23:36:51 -0600

    contact page created with forms

---
+++ finalproject/scripts/typewriter.js
@@ -0,0 +1,15 @@
+// Typewriter effect
+const text = "Welcome to my website!";
+  let i = 0;
+
+  function typeWriter() {
+    if (i < text.length) {
+      document.getElementById("typeWriter").textContent += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100); 
+    } else {
+      document.getElementById("typeWriter").style.borderRight = "none";
+    }
+  }
+
+  typeWriter();
